f90540cde603c0ed62af7b659c9269d1264f2cb9
Mitren Chinoy <michinoy@microsoft.com>
2020-09-17 15:54:19 -0700

    sample repo

---
+++ HelloWorldMvc/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HelloWorldMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+        public ActionResult Index()
+        {
+            Trace.TraceError("Test Error message!");
+            return View();
+        }
+	}
+}
